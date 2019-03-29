2d8075883677a64b92e85105132fbc2c65845fbe
Leonardo Frasson <leonardo.frasson@intranet.optcode.com.br>
2019-03-29 11:44:10 -0300

    Adicionado Projeto Angula SPA

---
+++ ProAgil-App/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      EventosComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
